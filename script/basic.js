c0ae2fef689e753311ffa1954b07d84884882624
_AlfianoLukita <alfianolukita@gmail.com>
2025-11-10 21:27:26 +0700

    New .js file
    basic.js : for Basic features
    main.js : for like sound click and More in the next update

--- script/basic.js
+++ script/basic.js
@@ -1,1 +1,44 @@
 
+var Num = 1;
+var Count = 0;
+    
+function plus() {
+  Count += Num;
+  document.getElementById("counter").innerText = Count;
+      
+}
+function minus() {
+  Count -= Num;
+  document.getElementById("counter").innerText = Count;
+}
+function reset() {
+  Count = 0;
+  Num = 1;
+  document.getElementById("counter").innerText = Count;
+  document.getElementById("Config").innerText = Num;
+}
+function Add() {
+  Num++;
+  document.getElementById("Config").innerText = Num;
+}
+function Add5() {
+  Num += 5;
+  document.getElementById("Config").innerText = Num;
+}
+function Add10() {
+  Num += 10;
+  document.getElementById("Config").innerText = Num;
+}
+function Subtract() {
+  Num--;
+  document.getElementById("Config").innerText = Num;
+}
+function Subtract5() {
+  Num -= 5 ;
+  document.getElementById("Config").innerText = Num;
+}
+function Subtract10() {
+  Num -= 10 ;
+  document.getElementById("Config").innerText = Num;
+}
+
--- script/main.js
+++ script/main.js
@@ -1,1 +1,44 @@
+const Click = new Audio('media/clicksoundeffect.mp3');
+const Btn = document.querySelectorAll('button');
+const Sfx = document.getElementById('SFX');
+Btn.forEach(button => {
+  button.addEventListener("click", () => { 
+    if(Sfx.checked){
+      Click.currentTime = 0; 
+      Click.play();
+    }else {
+      Click.pause()
+      Click.currentTime = 0;
+    }
+  });
+});
 
+function DarkTheme(){
+  
+  document.body.style.backgroundColor = "#2a2a2a";
+  document.body.style.color = "white";
+  const Para = document.querySelectorAll("p");
+  const Btn = document.querySelectorAll("button:not(#No)");
+  
+  Para.forEach(p => {
+    p.style.color = "white";
+  });
+  Btn.forEach(button => {
+    button.style.color = "#2a2a2a";
+    button.style.backgroundColor = "white";
+  });
+};
+function LightTheme(){
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "#2a2a2a";
+  
+  const Para = document.querySelectorAll("p");
+  const Btn = document.querySelectorAll("button:not(#No)");
+  Para.forEach(p => {
+    p.style.color = "#2a2a2a";
+  });
+  Btn.forEach(button => {
+    button.style.color = "white";
+    button.style.backgroundColor = "#2a2a2a";
+  });
+};
